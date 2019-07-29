772aba9eb51cf61620e44a0de1cd43fc509bafa2
trongdth <trongdth@gmail.com>
2019-07-29 23:22:15 +0700

    Update missing page

---
+++ lib/screens/activity.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter/material.dart';
+
+class ActivityScreen extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }
+
+}
---
+++ lib/screens/signup.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter/material.dart';
+
+class SignUpScreen extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() {
+    return null;
+  }
+
+}
