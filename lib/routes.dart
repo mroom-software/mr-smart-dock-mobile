224a41134c9baf90e6845bbd8d715a0d1e6716d7
trongdth <trongdth@gmail.com>
2019-07-29 13:58:55 +0700

    Update

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,6 +1,6 @@
 
 import 'package:flutter/material.dart';
-import 'package:smart_dock_mobile/screens/setup.dart';
+import 'package:smart_dock_mobile/screens/login.dart';
 
 class Routes extends StatelessWidget {
 
@@ -10,17 +10,20 @@ class Routes extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         backgroundColor: Color.fromRGBO(248, 249, 250, 1),
-        primaryColor: Color.fromRGBO(51, 51, 51, 1),
+        primaryColor: Color.fromRGBO(81, 81, 81, 1),
+        accentColor: Color.fromRGBO(207, 207, 207, 1),
+        buttonColor: Colors.white,
         fontFamily: 'Avenir',
         textTheme: TextTheme(
           headline: TextStyle(fontSize: 22.0, fontStyle: FontStyle.normal,),
           body1: TextStyle(fontSize: 15.0, fontStyle: FontStyle.normal,),
           title: TextStyle(fontSize: 18.0, fontStyle: FontStyle.normal,),
           subtitle: TextStyle(fontSize: 13.0, fontStyle: FontStyle.normal,),
+          button: TextStyle(fontSize: 16.0, fontStyle: FontStyle.normal, color: Theme.of(context).buttonColor),
         ),
       ),
       routes: {
-        '/': (context) => SetupScreen(),
+        '/': (context) => LoginScreen(),
       },
     );
   }
