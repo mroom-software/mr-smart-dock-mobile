a45fbf22042d7acaab04290d71128ffe2bd2010a
trongdth <trongdth@gmail.com>
2019-08-02 09:05:05 +0700

    Update sign in screen

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -5,7 +5,33 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return null;
+    return Scaffold(
+      appBar: null,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Expanded(
+              flex: 1,
+              child: Align(
+                child: Image.asset('assets/top.png'),
+                alignment: Alignment.topCenter,
+              ),
+            ),
+            Expanded(
+              flex: 5,
+              child: Align(
+                alignment: Alignment.topCenter,
+                child: Text(
+                  'Sign In'
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+    
   }
 
 }
