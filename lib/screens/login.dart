0779a9dd9102fd0b83bdfbd944ac9fcc2a9fbb45
trongdth <trongdth@gmail.com>
2019-08-03 17:48:16 +0700

    [Login screen] Update UI

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -27,16 +27,75 @@ class LoginScreen extends StatelessWidget {
                         style: Theme.of(context).textTheme.headline,
                       ),
                     ),
+                    SizedBox(
+                      height: 10,
+                    ),
                     Container(
                       decoration: BoxDecoration(
                         border: Border.all(color: Theme.of(context).accentColor),
                         borderRadius: BorderRadius.all(Radius.circular(4.0)),
                       ),
-                      child: TextFormField(
-                        decoration: InputDecoration(
-                          border: InputBorder.none,
-                          hintText: 'Email',
+                      child: Padding(
+                        padding: const EdgeInsets.all(5),
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: 'Email',
+                          ),
                         ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Theme.of(context).accentColor),
+                        borderRadius: BorderRadius.all(Radius.circular(4.0)),
+                      ),
+                      child: Row(
+                        children: <Widget>[
+                          Expanded(
+                            flex: 8,
+                            child: Padding(
+                              padding: const EdgeInsets.all(5),
+                              child: TextFormField(
+                                decoration: InputDecoration(
+                                  border: InputBorder.none,
+                                  hintText: 'Your Password',
+                                ),
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            flex: 2,
+                            child: Container(
+                              height: 30,
+                              margin: const EdgeInsets.symmetric(horizontal: 10),
+                              decoration: BoxDecoration(
+                                color: Color(0xFFF5F5F5),
+                                border: Border.all(color: Color(0xFFF5F5F5)),
+                                borderRadius: BorderRadius.all(Radius.circular(4.0)),
+                              ),
+                              child: Center(
+                                child: InkWell(
+                                  onTap: () {
+                                    
+                                  },
+                                  child: Text(
+                                    'SHOW',
+                                    style: TextStyle(
+                                      fontSize: 13.0,
+                                      fontWeight: FontWeight.w500,
+                                      color: Color(0xFF003BFF),
+                                    ),
+                                  ),
+                                ), 
+                              ),
+                            ), 
+                          ),
+                          
+                        ],
                       ),
                     ),
                   ],
