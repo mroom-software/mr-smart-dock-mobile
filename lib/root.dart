e9d70d5b5cd13557776e68493692bd66e0e72315
trongdth <trongdth@gmail.com>
2019-08-13 14:32:19 +0700

    [WIP] add auth provider

---
+++ lib/root.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Root extends StatefulWidget {
+  @override
+  _RootState createState() => _RootState();
+}
+
+class _RootState extends State<Root> {
+
+  @override
+  void initState() {
+    
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
